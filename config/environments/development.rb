63946e872a0b109353a303e4ea959ec4f4262398
Tibor Szolár <tibor.szolar@seznam.cz>
2014-03-26 20:01:23 +0100

    ActiveRecord load error fix.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,9 @@ Sirius::Application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations
-  config.active_record.migration_error = :page_load
+  #
+  # Disabled because of Sequel.
+  #config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
