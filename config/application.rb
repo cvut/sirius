760ee8c9e385926cd13b6b6a04090dc3b58099b6
Tibor Szolár <tibor.szolar@seznam.cz>
2014-03-26 20:01:00 +0100

    Timestamps support in Sequel.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,9 @@ module Sirius
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.sequel.after_connect = proc do
+      Sequel::Model.plugin :timestamps, update_on_create: true
+    end
   end
 end
