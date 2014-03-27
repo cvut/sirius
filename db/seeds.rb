531e6952e94dbb4f87c1203636dabe672ffcaf59
Tibor Szolar <tibor.szolar@seznam.cz>
2014-03-27 14:44:33 +0100

    First seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.create({code: 'MI-PSL', department: '18102', name: Sequel.hstore({cs:'Programování v jazyku Scala', en: 'Programming in Scala'})})
+
