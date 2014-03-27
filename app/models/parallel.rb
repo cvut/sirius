864c69265bbb18af5486511c099c5e4a8aba0ad4
Tibor Szolar <tibor.szolar@seznam.cz>
2014-03-27 14:41:45 +0100

    Parallel scafolded.

---
+++ app/models/parallel.rb
@@ -0,0 +1,3 @@
+class Parallel < Sequel::Model
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,20 @@ Sequel.migration do
       
       primary_key [:filename]
     end
+    
+    create_table(:parallels) do
+      primary_key :id
+      column :kos_id, "text"
+      column :type, "text"
+      foreign_key :course_id, :courses, :key=>[:id]
+      column :code, "integer"
+      column :capacity, "integer"
+      column :occupied, "integer"
+      column :semester, "text"
+      column :teacher, "text"
+      column :created_at, "timestamp without time zone"
+      column :updated_at, "timestamp without time zone"
+    end
   end
 end
 Sequel.migration do
@@ -32,5 +46,6 @@ Sequel.migration do
     self << "INSERT INTO \"schema_migrations\" (\"filename\") VALUES ('20140326184848_create_events.rb')"
     self << "INSERT INTO \"schema_migrations\" (\"filename\") VALUES ('20140327012704_create_courses.rb')"
     self << "INSERT INTO \"schema_migrations\" (\"filename\") VALUES ('20140327125618_add_timestamps_to_courses.rb')"
+    self << "INSERT INTO \"schema_migrations\" (\"filename\") VALUES ('20140327132523_create_parallels.rb')"
   end
 end
