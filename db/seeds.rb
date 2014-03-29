39b1e7035cb9c72a90b7231be41bdcc1874ee6cb
Tibor Szolar <tibor.szolar@seznam.cz>
2014-03-29 13:49:49 +0100

    Some sample seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 Course.create({code: 'MI-PSL', department: '18102', name: Sequel.hstore({cs:'Programování v jazyku Scala', en: 'Programming in Scala'})})
 
+Event.create({name:'MI-PSL 1. cvičení', starts_at: DateTime.strptime('03/05/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/05/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 1})
+Event.create({name:'MI-PSL 2. cvičení', starts_at: DateTime.strptime('03/12/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/12/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 2})
+Event.create({name:'MI-PSL 3. cvičení', starts_at: DateTime.strptime('03/19/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/19/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 3})
+
