d75da56c03cfe33c64720fbc3a9cb74071226c3a
Tibor Szolár <tibor.szolar@seznam.cz>
2014-03-29 13:59:11 +0100

    Event Planner cleanup.

--- lib/sirius/schedule_calendar_planner.rb
+++ lib/sirius/schedule_calendar_planner.rb
@@ -12,35 +12,14 @@ module Sirius
       week_offset = 0.weeks
       week_offset = 1.week if parallel[:parity] != :both && parallel[:parity] != @first_week_parity
 
-      event_start = @schedule_start + parallel[:start_time].seconds_since_midnight.seconds + week_offset
+      schedule_start = @schedule_start + parallel[:start_time].seconds_since_midnight.seconds + week_offset
       event_duration = (parallel[:end_time] - parallel[:start_time])
-      event_schedule = IceCube::Schedule.new(event_start, duration: event_duration)
+      event_schedule = IceCube::Schedule.new(schedule_start, duration: event_duration)
       event_schedule.add_recurrence_rule to_recurrence_rule(parallel)
       event_schedule.all_occurrences.map{ |event_start| { starts_at: event_start, ends_at: event_start + event_duration } }
     end
 
     private
-    def day_to_offset(day_of_the_week)
-      case day_of_the_week
-        when :monday
-          0
-        when :tuesday
-          1
-        when :wednesday
-          2
-        when :thursday
-          3
-        when :friday
-          4
-        when :saturday
-          5
-        when :sunday
-          6
-        else
-          raise "Unknown day #{day_of_the_week}"
-      end
-    end
-
     def to_recurrence_rule(parallel)
       week_frequency = 1 #every week by default
       week_frequency = 2 if parallel[:parity] != :both
