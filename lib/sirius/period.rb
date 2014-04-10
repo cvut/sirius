1d681820df498ab78ed87fb6440fa2c9254b8c80
Tibor Szolar <tibor.szolar@seznam.cz>
2014-04-10 19:53:43 +0200

    Simple Period class for wrapping start-end date tuples.

---
+++ lib/sirius/period.rb
@@ -0,0 +1,22 @@
+module Sirius
+  class Period
+
+    attr_reader :starts_at, :ends_at
+
+    def initialize(starts_at, ends_at)
+
+      @starts_at = starts_at
+      @ends_at = ends_at
+    end
+
+
+    def ==(other)
+      @starts_at == other.starts_at && @ends_at == other.ends_at
+    end
+
+    def self.parse(start_str, end_str)
+      Period.new(Time.parse(start_str), Time.parse(end_str))
+    end
+
+  end
+end
