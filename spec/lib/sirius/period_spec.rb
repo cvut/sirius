6b90d3e996657e16c3aec23456b9f2fb1653e8b6
Tibor Szolar <tibor.szolar@seznam.cz>
2014-04-14 11:02:38 +0200

    Period spec tidied up.

--- spec/lib/sirius/period_spec.rb
+++ spec/lib/sirius/period_spec.rb
@@ -2,28 +2,35 @@ require 'spec_helper'
 
 describe Sirius::Period do
 
-  it 'parses two time values' do
-    period = Sirius::Period.parse('7:30', '9:00')
-    expect(period.starts_at).to eq(Time.parse('7:30'))
-    expect(period.ends_at).to eq(Time.parse('9:00'))
+  describe '#parse' do
+
+    it 'parses time values' do
+      period = Sirius::Period.parse('7:30', '9:00')
+      expect(period.starts_at).to eq(Time.parse('7:30'))
+      expect(period.ends_at).to eq(Time.parse('9:00'))
+    end
+
+    it 'parses date values' do
+      period = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
+      expect(period.starts_at).to eq(Time.parse('11. 12. 2010'))
+      expect(period.ends_at).to eq(Time.parse('15. 4. 2011'))
+    end
+
   end
 
-  it 'parses two date values' do
-    period = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
-    expect(period.starts_at).to eq(Time.parse('11. 12. 2010'))
-    expect(period.ends_at).to eq(Time.parse('15. 4. 2011'))
-  end
+  describe '#==' do
 
-  it 'compares positively with equal instance' do
-    period_a = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
-    period_b = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
-    expect(period_a == period_b).to be_truthy
-  end
+    it 'compares positively with equal instance' do
+      period_a = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
+      period_b = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
+      expect(period_a == period_b).to be_truthy
+    end
 
-  it 'compares negatively with nonequal instance' do
-    period_a = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
-    period_b = Sirius::Period.parse('11. 12. 2010', '16. 4. 2011')
-    expect(period_a == period_b).to be_falsey
+    it 'compares negatively with nonequal instance' do
+      period_a = Sirius::Period.parse('11. 12. 2010', '15. 4. 2011')
+      period_b = Sirius::Period.parse('11. 12. 2010', '16. 4. 2011')
+      expect(period_a == period_b).to be_falsey
+    end
   end
 
 end
