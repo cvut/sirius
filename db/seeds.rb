f2d2c6b0f268d8774fdab47a553174bd1f49ee09
Tibor Szolar <tibor.szolar@seznam.cz>
2014-04-30 18:28:20 +0200

    More seed data for exploratory testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Course.create({code: 'MI-PSL', department: '18102', name: Sequel.hstore({cs:'Programování v jazyku Scala', en: 'Programming in Scala'})})
+scala = Course.create(code: 'MI-PSL', department: '18102', name: Sequel.hstore({cs:'Programování v jazyku Scala', en: 'Programming in Scala'}))
+
+room = Room.create({code: 'T9:350'})
 
 Event.create({name:'MI-PSL 1. cvičení', starts_at: DateTime.strptime('03/05/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/05/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 1})
 Event.create({name:'MI-PSL 2. cvičení', starts_at: DateTime.strptime('03/12/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/12/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 2})
 Event.create({name:'MI-PSL 3. cvičení', starts_at: DateTime.strptime('03/19/2014 9:15', '%m/%d/%Y %H:%M'), ends_at: DateTime.strptime('03/19/2014 10:45', '%m/%d/%Y %H:%M'), sequence_number: 3})
+
+scala_101 = Parallel.create({code: 101, capacity: 24, occupied: 24, semester: 'B132', course_id: scala.id})
+scala_102 = Parallel.create({code: 101, capacity: 24, occupied: 24, semester: 'B132', course_id: scala.id})
+
+TimetableSlot.create({day: 1, parity: 0, first_hour: 1, duration: 2, room_id: room.id, parallel_id: scala_101.id})
+TimetableSlot.create({day: 3, parity: 1, first_hour: 6, duration: 2, room_id: room.id, parallel_id: scala_102.id})
 
