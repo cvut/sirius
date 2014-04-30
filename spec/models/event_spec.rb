232aea535c83239ea9a09b4524567bc8b6abe161
Tibor Szolar <tibor.szolar@seznam.cz>
2014-04-30 20:41:23 +0200

    Added computed property "period" to Event entity.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Event do
-  skip "add some examples to (or delete) #{__FILE__}"
+
+  subject(:event) { Event.new(starts_at: Time.parse('11:00'), ends_at: Time.parse('12:30')) }
+  let(:period) { Sirius::Period.parse('7:30', '9:00') }
+
+  it 'returns correct calculated period' do
+    expect(event.period).to eq Period.parse('11:00', '12:30')
+  end
+
+  it 'sets starts_at and ends_at attributes from period=' do
+    event.period = period
+    expect(event.starts_at).to eq period.starts_at
+    expect(event.ends_at).to eq period.ends_at
+  end
+
 end
