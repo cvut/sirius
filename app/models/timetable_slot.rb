1c57dbefbf3dc268fbfc07931650bc13ca0ae06b
Tibor Szolar <tibor.szolar@seznam.cz>
2014-04-30 19:22:25 +0200

    Conversion of symbolic values to numeric and back in TimetableSlow when working with database. (subject to change)

--- app/models/timetable_slot.rb
+++ app/models/timetable_slot.rb
@@ -1,5 +1,33 @@
 class TimetableSlot < Sequel::Model
 
+  DAYS = [nil, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+  PARITIES = [:both, :odd, :even]
+
   many_to_one :parallel
+  many_to_one :room
+
+  def parity
+    PARITIES[super]
+  end
+
+  def parity=(new_parity)
+    numerical_parity = case new_parity
+                    when :odd then 1
+                    when :even then 2
+                    when :both then 0
+                    else raise "Invalid parity type #{new_parity}"
+                       end
+    super numerical_parity
+  end
+
+  def day
+    DAYS[super]
+  end
+
+  def day=(new_day)
+    day_index = DAYS.find_index(new_day)
+    raise "Invalid day name #{new_day}" if day_index.nil?
+    super day_index
+  end
 
 end
