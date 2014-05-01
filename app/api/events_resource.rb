49da3174b912844cf0d654824f1c40505798d71c
Jan Vlnas <git@jan.vlnas.cz>
2014-05-01 20:42:59 +0200

    Fix events resource

--- app/api/events_resource.rb
+++ app/api/events_resource.rb
@@ -12,36 +12,13 @@ module API
         represent ::Event.all, with: EventRepresenter
       end
 
-      desc 'Create an event'
-      params do
-        requires :name, type: String, desc: 'Name of the event'
-        requires :starts_at, type: DateTime, desc: 'Start time and date of the event'
-        requires :ends_at, type: DateTime, desc: 'End time and date of the event'
-      end
-      post do
-        event = Event.create(declared(params, include_missing: false))
-        # error!(present_error(:record_invalid, event.errors.full_messages)) unless <%= name.underscore %>.errors.empty?
-        error!(:record_invalid, 422) unless event.errors.empty? # event.errors.full_messages)
-        represent event, with: EventRepresenter
-      end
-
       params do
         requires :id, type: Integer, desc: 'ID of the event'
       end
       route_param :id do
         desc 'Get an event'
         get do
-          event = Event.find(params[:id])
-          represent event, with: EventRepresenter
-        end
-
-        desc 'Update an event'
-        params do
-          requires :id, desc: 'ID of the event to update'
-        end
-        put do
-          event = Event.find(params[:id])
-          event.update_attributes!(declared(params, include_missing: false))
+          event = Event[params[:id]]
           represent event, with: EventRepresenter
         end
       end
--- spec/api/events_resource_spec.rb
+++ spec/api/events_resource_spec.rb
@@ -2,40 +2,22 @@ require 'api_spec_helper'
 
 describe API::EventsResource do
   subject { response }
+  let(:event) { Fabricate(:event) }
   let(:status) { response.status }
-  let :event_attributes do
-    {
-      name: 'Test event',
-      starts_at: '2014-04-23T07:30:00Z',
-      ends_at: '2014-04-23T09:00:00Z'
-    }
-  end
 
   describe 'GET /events' do
     before { get '/events' }
 
     it 'returns a collection of events' do
-      expect(status).to eql 200
+      expect(status).to eql(200)
     end
   end
 
-  describe 'POST /events' do
-    before { post '/events', event_attributes, format: :json }
+  describe 'GET /events/:id' do
+    before { get "/events/#{event.id}", format: :json }
 
-    it 'creates an event' do
-      expect(status).to eql 201
+    it 'returns an event' do
+      expect(status).to eql(200)
     end
-
-    context 'with invalid data' do
-      let(:event_attributes) do
-        { name: 'Invalid' }
-      end
-
-      it 'returns an error' do
-        expect(status).to eql 422
-      end
-    end
-
-
   end
 end
