8a5ee583e5fc1a7a8260e423239460906b652f2a
Jan Vlnas <git@jan.vlnas.cz>
2014-05-02 00:04:58 +0200

    Preload all the stuff for development

--- config/development.rb
+++ config/development.rb
@@ -1,1 +1,5 @@
 DB.loggers = [Logger.new(STDOUT)]
+
+# Load ALL THE FILES!!!
+Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| load(f) }
+Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |f| load(f) }
