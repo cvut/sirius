69b1413b21685dbbec72d3ddd98ac2916e38f439
Tibor Szolar <tibor.szolar@seznam.cz>
2014-05-28 03:23:25 +0200

    New version of kosapi_client.

--- lib/sirius/schedule_manager.rb
+++ lib/sirius/schedule_manager.rb
@@ -3,6 +3,10 @@ require 'sirius/semester'
 module Sirius
   class ScheduleManager
 
+    def initialize(client = KOSapiClient.client)
+      @client = client
+    end
+
     def plan_stored_parallels
       semesters = load_semesters
       semesters.map do |sem|
@@ -14,7 +18,11 @@ module Sirius
     end
 
     def fetch_and_store_parallels
+      current_semester = 'B132'
+      parallels = @client.parallels.where('course.faculty' => 18000, semester: current_semester).offset(0).limit(20)
+      parallels.each do |parallel|
 
+      end
     end
 
 
@@ -22,8 +30,6 @@ module Sirius
     def load_semesters
       [Semester.new]
     end
-
-
 
   end
 end
