9efbf1fc39d4c6e81ab6f5bd341d8cdf6926ac7a
Jan Vlnas <git@jan.vlnas.cz>
2014-06-09 14:25:29 +0200

    Add ActiveModel compatibility

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -5,3 +5,9 @@ require 'sequel'
 db_config = YAML.load_file('config/database.yml')[RACK_ENV]
 DB = Sequel.connect(db_config)
 DB.extension :pg_hstore
+
+# Add ActiveModel compatibility
+# http://sequel.jeremyevans.net/rdoc-plugins/classes/Sequel/Plugins/ActiveModel.html
+# Used by ActiveModel Serializers, pagination
+require 'sequel/plugins/active_model'
+Sequel::Model.plugin :active_model
