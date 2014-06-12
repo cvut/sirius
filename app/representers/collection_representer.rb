c3b433b2ee8776b3f80463049b99400f16af2526
Jan Vlnas <git@jan.vlnas.cz>
2014-06-12 13:46:40 +0200

    EventRepresenter: define collection wrapping

--- app/representers/collection_representer.rb
+++ app/representers/collection_representer.rb
@@ -1,5 +1,7 @@
 require 'base_representer'
 require 'representable/json/collection'
 class CollectionRepresenter < BaseRepresenter
+  # FIXME: This will have to be changed from the lonely collection
+  # to support `meta` properties
   include Representable::JSON::Collection
 end
--- app/representers/events_representer.rb
+++ app/representers/events_representer.rb
@@ -1,6 +1,7 @@
 require 'collection_representer'
 require 'event_representer'
 class EventsRepresenter < CollectionRepresenter
+  self.representation_wrap = :events
   # TODO: See if EventRepresenter can be defined inline
   collection :events, decorator: EventRepresenter
 end
