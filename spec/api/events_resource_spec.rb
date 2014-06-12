60b7da3db17e8adeb446a1505cee552f3033d658
Jan Vlnas <git@jan.vlnas.cz>
2014-06-12 14:51:35 +0200

    Fix event_resource integration spec

--- spec/api/events_resource_spec.rb
+++ spec/api/events_resource_spec.rb
@@ -2,22 +2,48 @@ require 'api_spec_helper'
 
 describe API::EventsResource do
   subject { response }
-  let(:event) { Fabricate(:event) }
+  let!(:event) { Fabricate(:event) }
   let(:status) { response.status }
+  let(:body) { response.body }
+
+  let(:event_json) do
+    {
+      id: event.id,
+      name: event.name,
+      starts_at: event.starts_at,
+      ends_at: event.ends_at
+    }.to_json
+  end
 
   describe 'GET /events' do
+    let!(:events) { Fabricate.times(2, :event) }
     before { get '/events' }
 
-    it 'returns a collection of events' do
+    it 'returns OK' do
       expect(status).to eql(200)
+    end
+
+    context 'JSON-API format' do
+      subject { body }
+
+      it { should have_json_size(1+2).at_path('events') }
+
     end
   end
 
   describe 'GET /events/:id' do
-    before { get "/events/#{event.id}", format: :json }
+    context 'JSON-API format' do
+      before { get "/events/#{event.id}" }
+      subject { body }
 
-    it 'returns an event' do
-      expect(status).to eql(200)
+      it 'returns OK' do
+        expect(status).to eql(200)
+      end
+
+      it { should have_json_size(1).at_path('events') }
+
+      it { should be_json_eql(event_json).at_path('events/0') }
+
     end
   end
 end
