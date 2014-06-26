6265ad375a6e8db4f8f0c62f1f89ab6145b409a6
Jan Vlnas <git@jan.vlnas.cz>
2014-06-26 16:20:17 +0200

    Handle Ical Formatting through EventsRepresenter

--- app/representers/events_representer.rb
+++ app/representers/events_representer.rb
@@ -1,7 +1,14 @@
 require 'collection_representer'
 require 'event_representer'
+require 'events_ical_formatting'
+
 class EventsRepresenter < CollectionRepresenter
   self.representation_wrap = :events
   # TODO: See if EventRepresenter can be defined inline
   collection :events, decorator: EventRepresenter
+
+  # FIXME: this should be handled on Grape's level
+  def to_ical
+    EventsIcalFormatting.new.call(events)
+  end
 end
