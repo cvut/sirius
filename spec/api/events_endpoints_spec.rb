a1577ac3e717a9cbd1db3b603e5987d87b33ab65
Jan Vlnas <git@jan.vlnas.cz>
2014-07-16 20:01:58 +0200

    Extend Endpoints spec

--- spec/api/events_endpoints_spec.rb
+++ spec/api/events_endpoints_spec.rb
@@ -1,10 +1,12 @@
 require 'api_spec_helper'
+require 'icalendar'
 
 describe API::EventsEndpoints do
   subject { response }
   let!(:event) { Fabricate(:event) }
   let(:status) { response.status }
   let(:body) { response.body }
+  let(:headers) { response.headers }
 
   let(:event_json) do
     {
@@ -17,17 +19,40 @@ describe API::EventsEndpoints do
 
   describe 'GET /events' do
     let!(:events) { Fabricate.times(2, :event) }
-    before { get '/events' }
 
-    it 'returns OK' do
-      expect(status).to eql(200)
+    context 'with default parameters' do
+      before { get '/events' }
+
+      it 'returns OK' do
+        expect(status).to eql(200)
+      end
+
+      context 'JSON-API format' do
+        subject { body }
+
+        it { should have_json_size(1+2).at_path('events') }
+      end
+
     end
 
-    context 'JSON-API format' do
+    context 'with pagination' do
+      before { get '/events?limit=2&offset=1'}
       subject { body }
 
-      it { should have_json_size(1+2).at_path('events') }
+      it { should have_json_size(2).at_path('events') }
+    end
 
+    context 'as an icalendar' do
+      before { get '/events.ical' }
+
+      it 'returns a valid content-type' do
+        expect(headers['Content-Type']).to eql('text/calendar')
+      end
+
+      it 'returns a valid iCalendar' do
+        calendar = Icalendar.parse(body).first
+        expect(calendar.events.size).to eq 3
+      end
     end
   end
 
