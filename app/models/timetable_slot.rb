a81ebaa8de85323e7a32d5cd3025f41a146f930c
Tibor Szolár <tibor.szolar@seznam.cz>
2014-07-16 03:03:59 +0200

    TimetableSlot loading.

--- app/models/timetable_slot.rb
+++ app/models/timetable_slot.rb
@@ -3,8 +3,20 @@ require 'parity'
 
 class TimetableSlot < Sequel::Model
 
+  DB_KEYS = [:id, :day, :duration, :first_hour, :parity]
+
   many_to_one :parallel
   many_to_one :room
+
+  def self.from_kosapi(slot, parallel)
+    TimetableSlot.unrestrict_primary_key
+    slot_hash = slot.to_hash
+    slot_hash = slot_hash.select { |key,_| DB_KEYS.include? key }
+    db_slot = TimetableSlot.new(slot_hash)
+    db_slot.parallel_id = parallel[:id]
+    room_code = slot.room.title
+    db_slot.save()
+  end
 
   def parity
     Parity.from_numeric(super)
