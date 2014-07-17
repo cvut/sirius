35cb5eb0721f3542e7161c342a98becfe8b68bad
Jan Vlnas <git@jan.vlnas.cz>
2014-07-17 21:23:07 +0200

    Scratch that, refinements won't help

--- lib/refinements/object_wrap.rb
+++
@@ -1,9 +0,0 @@
-# Useful for wrapping object into a delegator
-# without breaking a chain.
-module ObjectWrap
-  refine Object do
-    def wrap_with(klass)
-      klass.new(self)
-    end
-  end
-end
--- spec/lib/refinements/method_chain_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'refinements/method_chain'
-
-# Based on https://github.com/gregwebs/methodchain/blob/master/spec/methodchain_spec.rb
-# Copyright (c) 2008 Greg Weber, http://gregweber.info
-# Licensed under the MIT license
-describe MethodChain do
-  pending 'Extract this to a separate fork & rewrite specs!'
-end
