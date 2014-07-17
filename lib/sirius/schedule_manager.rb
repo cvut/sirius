ab08bf3afbaf00f73fac6ad795657a55b40af9f5
Tibor Szolar <tibor.szolar@seznam.cz>
2014-07-17 21:21:15 +0200

    Implemented simple pagination when loading records from KOSapi.

--- lib/sirius/schedule_manager.rb
+++ lib/sirius/schedule_manager.rb
@@ -18,11 +18,19 @@ module Sirius
 
     end
 
-    def fetch_and_store_parallels
+    def fetch_and_store_parallels(parallels_limit = 20)
       current_semester = 'B132'
-      parallels = @client.parallels.where('course.faculty' => 18000, semester: current_semester).offset(0).limit(20)
-      parallels.each do |parallel|
-        Parallel.from_kosapi(parallel)
+      limit = 20
+      total_parallels = 0
+      offset = 0
+      parallels = fetch_parallels(0, limit, current_semester)
+      while parallels.count > 0 && total_parallels < parallels_limit
+        parallels.each do |parallel|
+          Parallel.from_kosapi(parallel)
+        end
+        total_parallels += parallels.count
+        offset += limit
+        parallels = fetch_parallels(offset, limit, current_semester)
       end
     end
 
@@ -30,6 +38,10 @@ module Sirius
     private
     def load_semesters
       [Semester.new]
+    end
+
+    def fetch_parallels(offset, limit, semester, faculty = 18000)
+      @client.parallels.where('course.faculty' => faculty, semester: semester).offset(offset).limit(limit)
     end
 
   end
