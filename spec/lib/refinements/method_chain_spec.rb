b628315ed2902f7ffcd8650cb5661621136796b1
Jan Vlnas <git@jan.vlnas.cz>
2014-07-17 19:21:49 +0200

    Add MethodChain refinement

---
+++ spec/lib/refinements/method_chain_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'refinements/method_chain'
+
+# Based on https://github.com/gregwebs/methodchain/blob/master/spec/methodchain_spec.rb
+# Copyright (c) 2008 Greg Weber, http://gregweber.info
+# Licensed under the MIT license
+describe MethodChain do
+  pending 'Extract this to a separate fork & rewrite specs!'
+end
