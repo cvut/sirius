6bd80d870cfa739c5b1af947e567aa29bd5c9819
Tibor Szolar <tibor.szolar@seznam.cz>
2014-07-24 16:55:33 +0200

    UpdatedParallelsFinder now accepts since argument in find_updated method.

--- spec/lib/sirius/updated_parallels_finder_spec.rb
+++ spec/lib/sirius/updated_parallels_finder_spec.rb
@@ -4,10 +4,10 @@ require 'sirius/updated_parallels_finder'
 describe Sirius::UpdatedParallelsFinder, :vcr do
 
   let(:since) { Time.parse('1.7.2014') }
-  subject(:finder) { Sirius::UpdatedParallelsFinder.new(since: since, client: create_kosapi_client) }
+  subject(:finder) { Sirius::UpdatedParallelsFinder.new(client: create_kosapi_client) }
 
   it 'finds parallels updated since' do
-    parallels = finder.find_updated
+    parallels = finder.find_updated(since)
     expect(parallels.any? { |par| par.updated > since }).to be_truthy
   end
 end
