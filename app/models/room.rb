1b369bfe57092713163fc6f789233f30901172d0
Jan Vlnas <git@jan.vlnas.cz>
2014-07-31 00:17:21 +0200

    Add Room.with_code! method

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,10 @@ class Room < Sequel::Model
 
   one_to_many :events
 
+  ##
+  # Return a room with a given kos_code or raise an error
+  def self.with_code! kos_code
+    first!(kos_code: kos_code)
+  end
+
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,11 +3,24 @@ require 'models/room'
 
 describe Room do
 
+  subject(:room) { Room.create(kos_code: 'A-1442').reload }
+
   it 'has working timestamps' do
-    room = Room.new(kos_code: 'A-1442')
-    room.save
     expect(room.created_at).not_to be_nil
     expect(room.updated_at).to eq room.created_at
+  end
+
+  describe '.with_code!' do
+    it 'fails for non-existent room' do
+      expect { Room.with_code!('YOLO') }.to raise_error(Sequel::NoMatchingRow )
+    end
+
+    context 'with existing room' do
+      before { room }
+      it 'returns an existing room' do
+        expect(Room.with_code! 'A-1442').to eql room
+      end
+    end
   end
 
 end
