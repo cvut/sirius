6ba294fcb8c0b0573d0068421c0981537b756e34
Tibor Szolar <tibor.szolar@seznam.cz>
2014-08-06 17:27:46 +0200

    Removed manual pagination from ScheduleManager.

--- lib/sirius/schedule_manager.rb
+++ lib/sirius/schedule_manager.rb
@@ -19,19 +19,13 @@ module Sirius
     end
 
     def fetch_and_store_parallels(parallels_limit = 20)
-      current_semester = 'B132'
-      limit = 20
       total_parallels = 0
-      offset = 0
-      parallels = fetch_parallels(0, limit, current_semester)
+      parallels = fetch_parallels(0, 20, 'B132')
       DB.transaction do
-        while parallels.count > 0 && total_parallels < parallels_limit
-          parallels.each do |parallel|
-            Parallel.from_kosapi(parallel)
-          end
-          total_parallels += parallels.count
-          offset += limit
-          parallels = fetch_parallels(offset, limit, current_semester)
+        parallels.take_while do |parallel|
+          Parallel.from_kosapi(parallel)
+          total_parallels += 1
+          total_parallels < parallels_limit
         end
       end
     end
