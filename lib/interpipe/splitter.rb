2ba7b13fc637bb3770b884f24ea4fe6fa5d31100
Jan Vlnas <git@jan.vlnas.cz>
2014-08-14 18:16:33 +0200

    Rewrite Interpipe::Splitter as a subclass of Organizer

--- lib/interpipe/splitter.rb
+++ lib/interpipe/splitter.rb
@@ -1,35 +1,10 @@
-require 'interpipe/interactor'
+require 'interpipe/organizer'
 
 module Interpipe
-  module Splitter
-    def self.included(base)
-      base.class_eval do
-        include Interactor
-
-        extend ClassMethods
-        include InstanceMethods
-      end
-    end
-
-    module ClassMethods
-      def interactors
-        @interactors ||= []
-      end
-
-      def split(*interactors)
-        @interactors = interactors.flatten
-      end
-    end
-
-    module InstanceMethods
-      def interactors
-        self.class.interactors
-      end
-
-      def perform(**options)
-        @results = interactors.map do |interactor|
-          interactor.perform(options).results
-        end
+  class Splitter < Organizer
+    def perform(**options)
+      @results = interactors.map do |interactor|
+        interactor.perform(options).results
       end
     end
   end
