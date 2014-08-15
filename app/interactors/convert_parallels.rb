3be463cd7ff14b69fb9470a9b0aa36c5c193b1a7
Tibor Szolar <tibor.szolar@seznam.cz>
2014-08-15 04:35:30 +0200

    Re-implemented Parallel conversion with Interactors.

---
+++ app/interactors/convert_parallels.rb
@@ -0,0 +1,63 @@
+require 'interpipe/interactor'
+require 'roles/parallel_from_kosapi'
+
+class ConvertParallels
+  include Interpipe::Interactor
+
+  DB_KEYS = [:code, :parallel_type, :capacity, :occupied]
+
+  def setup
+    @parallels = []
+    @timetable_slots = {}
+    @people = {}
+    @courses = {}
+  end
+
+  def perform(kosapi_parallels)
+    @parallels = kosapi_parallels.map do |kosapi_parallel|
+      teachers = extract_teachers(kosapi_parallel)
+      course = extract_course(kosapi_parallel)
+      extract_slots(kosapi_parallel)
+      build_parallel(kosapi_parallel, teachers, course)
+    end
+  end
+
+  def build_parallel(kosapi_parallel, teachers, course)
+    parallel_hash = kosapi_parallel.to_hash
+    parallel_hash.select! { |key,_| DB_KEYS.include? key }
+    parallel_hash[:semester] = kosapi_parallel.semester.link_id
+    parallel = Parallel.new(parallel_hash) { |p| p.id = kosapi_parallel.link.link_id }
+    parallel.teacher_ids = teachers
+    parallel.course = course
+    parallel
+  end
+
+  def extract_teachers(kosapi_parallel)
+    kosapi_parallel.teachers.map do |teacher_link|
+      username = teacher_link.link_id
+      person = @people[username] ||= Person.new(full_name: teacher_link.link_title) { |p| p.id = username}
+      person.id
+    end
+  end
+
+  def extract_course(kosapi_parallel)
+    kosapi_course = kosapi_parallel.course
+    course_id = kosapi_course.link_id
+    @courses[course_id] ||= Course.new(name: Sequel.hstore({cs: kosapi_course.link_title})) { |c| c.id = course_id }
+  end
+
+  def extract_slots(kosapi_parallel)
+    parallel_id = kosapi_parallel.link.link_id
+    @timetable_slots[parallel_id] = kosapi_parallel.timetable_slots
+  end
+
+  def results
+    {
+        parallels: @parallels,
+        timetable_slots: @timetable_slots,
+        people: @people.values,
+        courses: @courses.values
+    }
+  end
+
+end
---
+++ spec/interactors/convert_parallels_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+require 'interactors/convert_parallels'
+require 'roles/parallel_from_kosapi'
+
+describe ConvertParallels do
+
+  subject(:convert) { described_class }
+
+  describe '#perform' do
+
+    context 'when parallels empty' do
+      it 'returns empty result' do
+        results = convert.perform([]).results
+        expect(results).to eq({parallels: [], timetable_slots: {}, people: [], courses: []})
+      end
+    end
+
+    context 'with parallels' do
+
+      let(:slot) { double(:slot) }
+      let(:teacher) { double(:teacher, link_id: 'vomackar', link_title: 'Ing. Karel Vomáčka CSc.') }
+      let(:parallel_attrs) { {id: 239018, parallel_type: :lecture, code: 1234, capacity: 50, occupied: 10} }
+      let(:course) { double(:course, link_id: 'BI-ZUM', link_title: 'Course title') }
+      let(:semester) { double(:semester, link_id: 'B102') }
+      let(:parallel) { double(:kosapi_parallel, to_hash: parallel_attrs, link: double(link_id: '239018'), semester: semester, course: course, timetable_slots: [slot], teachers: [teacher]) }
+
+      it 'converts parallels' do
+        results = convert.perform([parallel]).results
+        parallel = results[:parallels].first
+        expect(parallel.parallel_type).to eq 'lecture'
+        expect(parallel.code).to eq 1234
+        expect(parallel.capacity).to eq 50
+        expect(parallel.occupied).to eq 10
+        expect(parallel.id).to eq 239018
+      end
+
+      it 'extracts courses' do
+        results = convert.perform([parallel]).results
+        parallel = results[:parallels].first
+        course = results[:courses].first
+        expect(parallel.course_id).to eq course.id
+        expect(course.id).to eq 'BI-ZUM'
+        expect(course.name).to eq({'cs' => 'Course title'})
+      end
+
+      it 'extracts people' do
+        results = convert.perform([parallel]).results
+        parallel = results[:parallels].first
+        person = results[:people].first
+        expect(parallel.teacher_ids).to eq ['vomackar']
+        expect(person.id).to eq 'vomackar'
+        expect(person.full_name).to eq 'Ing. Karel Vomáčka CSc.'
+      end
+
+      it 'collect slots' do
+        results = convert.perform([parallel]).results
+        parallel = results[:parallels].first
+        expect(results[:timetable_slots]).to eq({'239018' => [slot]})
+      end
+
+    end
+
+  end
+
+end
