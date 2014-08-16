3b87c3ef737319c8e809b44c6b60d254f18dfa1d
Tibor Szolar <tibor.szolar@seznam.cz>
2014-08-16 04:54:29 +0200

    Implemented ConvertRoom Interactor for extracting Rooms from TimetableSlots.

---
+++ app/interactors/convert_rooms.rb
@@ -0,0 +1,28 @@
+require 'interpipe/interactor'
+
+class ConvertRooms
+  include Interpipe::Interactor
+
+  def setup
+    @rooms = {}
+  end
+
+  def perform(timetable_slots:, **options)
+    timetable_slots.each do |parallel_id, slots|
+      slots.each { |slot| convert_room(slot.room) }
+    end
+    @options = options
+  end
+
+  def results
+    { rooms: @rooms.values }.merge(@options)
+  end
+
+  private
+  def convert_room(room)
+    return if !room || room.link_title == 'no-title'
+    room_code = room.link_title
+    @rooms[room_code] ||= Room.new(kos_code: room_code)
+  end
+
+end
