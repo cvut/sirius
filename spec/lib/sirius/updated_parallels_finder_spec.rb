bf3c94cc4b6a1dae39334a0d0d2730575479e4c8
Tibor Szolar <tibor.szolar@seznam.cz>
2014-08-17 03:26:45 +0200

    Added optional till parameter to UpdateParallelsFinder.

--- spec/lib/sirius/updated_parallels_finder_spec.rb
+++ spec/lib/sirius/updated_parallels_finder_spec.rb
@@ -4,10 +4,19 @@ require 'sirius/updated_parallels_finder'
 describe Sirius::UpdatedParallelsFinder, :vcr do
 
   let(:since) { Time.parse('1.7.2014') }
+  let(:till) { Time.parse('10.7.2014') }
   subject(:finder) { Sirius::UpdatedParallelsFinder.new(client: create_kosapi_client) }
 
   it 'finds parallels updated since' do
     parallels = finder.find_updated(since)
+    parallels.auto_paginate = false
     expect(parallels.any? { |par| par.updated > since }).to be_truthy
   end
+
+  it 'finds parallels updated between' do
+    parallels = finder.find_updated(since, till)
+    expect(parallels.any? { |par| par.updated > since && par.updated < till }).to be_truthy
+  end
+
+
 end
