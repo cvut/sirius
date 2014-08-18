833cae2069ebddd36b5510ceafacc935074d3f1f
Jan Vlnas <git@jan.vlnas.cz>
2014-08-18 21:43:06 +0200

    Store offset, limit and count on filter_events

--- app/interactors/filter_events.rb
+++ app/interactors/filter_events.rb
@@ -12,16 +12,28 @@ class FilterEvents
   include RolePlaying::Context
   include Interpipe::Interactor
 
-  attr_reader :events
+  attr_reader :events, :limit, :offset
+
+  DEFAULT_LIMIT = 20
 
   def perform(events: , params: {}, format: :jsonapi)
     @events = DateFilteredDataset.played_by(events) do |dataset|
       dataset
         .filter_by_date(from: params[:from], to: params[:to])
-        .then_if(paginate?(format)) do |d|
-          PaginatedDataset.new(d).paginate(offset: params[:offset], limit: params[:limit])
-        end
+        .tap {|d|
+          @count = nil
+          @count_query = d
+        }
+        .then_if(paginate?(format)) { |d|
+          @offset = params[:offset] || 0
+          @limit = params[:limit] || DEFAULT_LIMIT
+          PaginatedDataset.new(d).paginate(offset: @offset, limit: @limit)
+        }
     end
+  end
+
+  def count
+    @count ||= @count_query.count
   end
 
   private
