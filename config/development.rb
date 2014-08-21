5b5fd04f34f2b87468976a0fdbdf218a30b84b42
Jan Vlnas <git@jan.vlnas.cz>
2014-08-21 20:54:16 +0200

    Remove unused files

--- config/development.rb
+++
@@ -1,5 +0,0 @@
-DB.loggers = [Logger.new(STDOUT)]
-
-# Load ALL THE FILES!!!
-Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| load(f) }
-Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |f| load(f) }
