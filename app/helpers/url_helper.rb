8b7e7b8cfe6799d374f16c2eaa7eb7a6edd818a7
Jan Vlnas <git@jan.vlnas.cz>
2014-09-03 19:53:35 +0200

    Add UrlHelper module with specs

---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,36 @@
+require 'uri'
+##
+# Based on https://github.com/emk/sinatra-url-for
+module UrlHelper
+
+  # Construct an absolute path to a given +url_fragment+,
+  # expects a leading slash.
+  def path_for url_fragment, **params
+    paramstr = ''
+    # TODO: Use addressable
+    unless params.empty?
+      paramstr += '?' + params.map { |k,v| "#{k}=#{URI.escape(v.to_s, /[^#{URI::PATTERN::UNRESERVED}]/)}" }.join('&')
+    end
+    "#{base_href}#{url_fragment}#{paramstr}"
+  end
+
+  # Construct a full URL to `url_fragment`, which should be given relative to
+  # the base of an API.
+  def url_for url_fragment, **params
+    "#{base_domain(absolute: true)}#{path_for url_fragment, params}"
+  end
+
+  def base_domain(absolute: false)
+    scheme = ''
+    if absolute
+      scheme += Config.force_ssl ? 'https://' : 'http://'
+    end
+    "#{scheme}#{Config.domain}"
+  end
+
+  # FIXME: This should/could use a context info or request.script_name if possible
+  def base_href
+    '/api/v1'
+  end
+
+end
---
+++ spec/helpers/url_helper_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'url_helper'
+
+describe UrlHelper do
+  subject(:helper) { Class.new { extend UrlHelper } }
+
+  describe '#base_domain' do
+    subject { helper.base_domain }
+    it { should eql 'example.com' }
+    context 'absolute' do
+      subject { helper.base_domain(absolute: true) }
+      context 'with FORCE_SSL' do
+        before do
+          allow(Config).to receive(:force_ssl).and_return(true)
+        end
+        it { should eql 'https://example.com'}
+      end
+      context 'without FORCE_SSL' do
+        before do
+          allow(Config).to receive(:force_ssl).and_return(false)
+        end
+        it { should eql 'http://example.com'}
+      end
+    end
+  end
+
+  describe '#base_href' do
+    subject { helper.base_href }
+    it { should eql '/api/v1' }
+  end
+
+  describe '#path_for' do
+    subject { helper.path_for '/foo' }
+    it { should eql '/api/v1/foo' }
+    context 'with params' do
+      subject(:path) { helper.path_for '/foo', bar: 'baz', quaz: 'quar'}
+      it 'should encode params' do
+        expect(path).to eql '/api/v1/foo?bar=baz&quaz=quar'
+      end
+    end
+  end
+
+  describe '#url_for' do
+    let(:fragment) { '/foo' }
+    subject { helper.url_for fragment }
+    before do
+      allow(Config).to receive(:force_ssl).and_return(false)
+    end
+
+    it { should eql 'http://example.com/api/v1/foo' }
+  end
+
+end
