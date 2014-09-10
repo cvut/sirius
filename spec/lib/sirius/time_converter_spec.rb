a1d0059600a6968b02590aa1a73eb1886ace2bec
Tibor Szolar <tibor.szolar@seznam.cz>
2014-09-10 20:13:12 +0200

    Improve teaching time conversion. Fixes #54.

--- spec/lib/sirius/time_converter_spec.rb
+++ spec/lib/sirius/time_converter_spec.rb
@@ -7,8 +7,25 @@ describe Sirius::TimeConverter do
   subject(:converter) { Sirius::TimeConverter.new(schedule_params) }
 
   it 'calculates event start and end time' do
-
     result = converter.convert_time(1, 2)
     expect(result).to eq(Period.parse('7:30','9:00'))
+  end
+
+  it 'calculates start and end of odd hours' do
+    result = converter.convert_time(12, 2)
+    expect(result).to eq(Period.parse('17:00','18:30'))
+  end
+
+  it 'calculates start and end of odd duration' do
+    result = converter.convert_time(13, 3)
+    expect(result).to eq(Period.parse('18:00','20:15'))
+  end
+
+  it 'raises error with invalid start time' do
+    expect { converter.convert_time(0, 2) }.to raise_error(RuntimeError)
+  end
+
+  it 'raises error with zero duration' do
+    expect { converter.convert_time(5, 0) }.to raise_error(RuntimeError)
   end
 end
