89e7857849c100200c456ee3774aa13642515338
Tibor Szolar <tibor.szolar@seznam.cz>
2014-09-11 18:01:52 +0200

    Event spec cleanup.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,14 +6,18 @@ describe Event do
   subject(:event) { Event.new(starts_at: Time.parse('11:00'), ends_at: Time.parse('12:30')) }
   let(:period) { Period.parse('7:30', '9:00') }
 
-  it 'returns correct calculated period' do
-    expect(event.period).to eq Period.parse('11:00', '12:30')
-  end
+  describe '#period' do
 
-  it 'sets starts_at and ends_at attributes from period=' do
-    event.period = period
-    expect(event.starts_at).to eq period.starts_at
-    expect(event.ends_at).to eq period.ends_at
+    it 'returns correct calculated period' do
+      expect(event.period).to eq Period.parse('11:00', '12:30')
+    end
+
+    it 'sets starts_at and ends_at attributes from period=' do
+      event.period = period
+      expect(event.starts_at).to eq period.starts_at
+      expect(event.ends_at).to eq period.ends_at
+    end
+
   end
 
   describe '.with_person' do
