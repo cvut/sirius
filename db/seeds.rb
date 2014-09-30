b87f8a0f89faaa9cac33bb33e3597b2166185873
Tibor Szolar <tibor.szolar@seznam.cz>
2014-09-30 19:08:46 +0200

    Refactor seeds.rb to behave idempotently.
    Also add current FEE faculty semester (update disabled by default).

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,12 @@
 
 require 'faculty_semester'
 
-FacultySemester.create(code: 'B141', faculty: 18000, update_enabled: true, first_week_parity: :odd,
-                        starts_at: '2014-09-22', teaching_ends_at: '2014-12-20', exams_start_at: '2015-01-05',
-                        exams_end_at: '2015-02-14', ends_at: '2015-02-14',
-                        hour_starts: %w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:30 14:30 15:15 16:15 17:00 18:00 18:45 19:45), hour_duration: 45)
+FacultySemester.find_or_create(code: 'B141', faculty: 18000) { |s| s.update_enabled = true, s.first_week_parity = :odd,
+    s.starts_at = '2014-09-22', s.teaching_ends_at = '2014-12-20', s.exams_start_at = '2015-01-05',
+    s.exams_end_at = '2015-02-14', s.ends_at = '2015-02-14',
+    s.hour_starts = %w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:30 14:30 15:15 16:15 17:00 18:00 18:45 19:45), s.hour_duration = 45 }
+
+FacultySemester.find_or_create(code: 'B141', faculty: 13000) { |s| s.update_enabled = false, s.first_week_parity = :odd,
+    s.starts_at = '2014-09-22', s.teaching_ends_at = '2014-12-20', s.exams_start_at = '2015-01-05',
+    s.exams_end_at = '2015-02-14', s.ends_at = '2015-02-14',
+    s.hour_starts = %w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:30 14:30 15:15 16:15 17:00 18:00 18:45 19:45), s.hour_duration = 45 }
