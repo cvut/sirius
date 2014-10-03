ebff85e1ac8a21c7e241888fd45be23209a3d318
Tibor Szolár <tibor.szolar@seznam.cz>
2014-10-03 02:31:22 +0200

    Remove unused Semester class.

--- lib/sirius/semester.rb
+++
@@ -1,9 +0,0 @@
-require 'sirius/time_converter'
-require 'sirius/semester_calendar'
-
-module Sirius
-  class Semester
-
-  end
-end
-
--- spec/lib/sirius/semester_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-require 'sirius/semester'
-
-describe Sirius::Semester do
-
-end
