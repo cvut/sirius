78050f06ca0fefdf9d2f71bd3ec288eda2b00423
Tibor Szolár <tibor.szolar@seznam.cz>
2014-10-03 02:29:55 +0200

    Introduce proper EventPlanner and move all event planning logic into it.

---
+++ lib/sirius/event_planner.rb
@@ -0,0 +1,41 @@
+require 'models/schedule_exception'
+require 'roles/applied_schedule_exception'
+require 'roles/planned_parallel'
+require 'roles/planned_timetable_slot'
+require 'models/parallel'
+require 'sirius/time_converter'
+require 'sirius/semester_calendar'
+
+module Sirius
+  class EventPlanner
+
+    def initialize
+      @sync = Sync[Event, matching_attributes: [:timetable_slot_id, :absolute_sequence_number]]
+      @exceptions = ScheduleException.all.map { |e| AppliedScheduleException.new(e) }
+    end
+
+    def plan_semester(semester)
+      time_converter, calendar_planner = create_converters(semester)
+      Parallel.eager_graph(:timetable_slots).all.each do |parallel|
+        parallel = PlannedParallel.new(parallel)
+        parallel.timetable_slots.each do |sl|
+          PlannedTimetableSlot.new(sl, time_converter, calendar_planner).tap do |slot|
+            events = slot.generate_events
+            events.each {|evt| @exceptions.each {|ex| ex.apply(evt) if ex.affects?(evt) } }
+            @sync.perform(events: events)
+            slot.clear_extra_events(events)
+          end
+        end
+        parallel.renumber_events
+      end
+    end
+
+    private
+    def create_converters(semester)
+      time_converter = TimeConverter.new(hour_starts: semester.hour_starts, hour_length: semester.hour_duration)
+      semester_calendar = SemesterCalendar.new(teaching_period: Period.new(semester.starts_at, semester.teaching_ends_at), first_week_parity: semester.first_week_parity)
+      [time_converter, semester_calendar]
+    end
+
+  end
+end
--- lib/sirius/schedule_manager.rb
+++ lib/sirius/schedule_manager.rb
@@ -1,12 +1,7 @@
-require 'sirius/semester'
-require 'models/parallel'
 require 'models/faculty_semester'
-require 'roles/planned_timetable_slot'
 require 'interactors/import_updated_parallels'
 require 'interactors/import_students'
-require 'models/schedule_exception'
-require 'roles/applied_schedule_exception'
-require 'roles/planned_parallel'
+require 'sirius/event_planner'
 
 module Sirius
   class ScheduleManager
@@ -16,22 +11,8 @@ module Sirius
     end
 
     def plan_stored_parallels
-      sync = Sync[Event, matching_attributes: [:timetable_slot_id, :absolute_sequence_number]]
-      exceptions = ScheduleException.all.map { |e| AppliedScheduleException.new(e) }
-      @active_semesters.each do |sem|
-        time_converter, calendar_planner = create_converters(sem)
-        Parallel.eager_graph(:timetable_slots).all.each do |parallel|
-          parallel = PlannedParallel.new(parallel)
-          parallel.timetable_slots.each do |sl|
-            PlannedTimetableSlot.new(sl, time_converter, calendar_planner).tap do |slot|
-              events = slot.generate_events
-              events.each {|evt| exceptions.each {|ex| ex.apply(evt) if ex.affects?(evt) } }
-              sync.perform(events: events)
-              slot.clear_extra_events(events)
-            end
-          end
-          parallel.renumber_events
-        end
+      @active_semesters.each do |semester|
+        EventPlanner.new.plan_semester(semester)
       end
     end
 
@@ -47,13 +28,6 @@ module Sirius
       DB.transaction do
         ImportStudents.perform
       end
-    end
-
-    private
-    def create_converters(semester)
-      time_converter = TimeConverter.new(hour_starts: semester.hour_starts, hour_length: semester.hour_duration)
-      semester_calendar = SemesterCalendar.new(teaching_period: Period.new(semester.starts_at, semester.teaching_ends_at), first_week_parity: semester.first_week_parity)
-      [time_converter, semester_calendar]
     end
 
   end
