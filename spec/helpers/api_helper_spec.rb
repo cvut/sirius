bd5dc432172319bab126e85b9e3536870425dab1
Jan Vlnas <git@jan.vlnas.cz>
2014-10-06 14:36:27 +0200

    Add ApiHelper specs for user_allowed?

--- spec/helpers/api_helper_spec.rb
+++ spec/helpers/api_helper_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
-
+require 'helpers/api_helper'
 describe ApiHelper do
-  subject(:helper) { Class.new { include ApiHelper } }
+  subject(:helper) { Class.new { extend ApiHelper } }
 
-  describe '#represent' do
+  describe '#user_allowed?' do
+    WardenMock = Struct.new(:user)
+    let(:auth_user) { '*' }
+    before do
+      allow(helper).to receive(:env) { {'warden' => WardenMock.new(auth_user)} }
+    end
+    context 'standard user' do
+      let(:auth_user) { 'vomackar' }
+
+      it 'grants authorization for matching username' do
+        expect(helper.user_allowed? 'vomackar').to be true
+      end
+
+      it 'prevents authorization for different username' do
+        expect(helper.user_allowed? 'skocdopet').to be false
+      end
+    end
   end
 
 end
