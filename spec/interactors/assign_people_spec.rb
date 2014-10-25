0b9b5094bf1551c56938f85a7cea839569b709e3
Tibor Szolár <tibor.szolar@seznam.cz>
2014-10-25 14:32:55 +0200

    Refactor AssignPeople spec to use brackets with `expect (not_)to change` matcher.

--- spec/interactors/assign_people_spec.rb
+++ spec/interactors/assign_people_spec.rb
@@ -10,50 +10,50 @@ describe AssignPeople do
     let(:event) { Fabricate(:event, parallel: parallel, semester: faculty_semester.code, faculty: faculty_semester.faculty, teacher_ids: []) }
 
     it 'assigns students from parallel to event' do
-      expect do
+      expect {
         described_class.perform(faculty_semester: faculty_semester)
         event.refresh
-      end.to change(event, :student_ids).to(%w(vomackar dude2))
+      }.to change(event, :student_ids).to(%w(vomackar dude2))
     end
 
     it 'assigns teachers from parallel to event' do
-      expect do
+      expect {
         described_class.perform(faculty_semester: faculty_semester)
         event.refresh
-      end.to change(event, :teacher_ids).to(%w(skocdop))
+      }.to change(event, :teacher_ids).to(%w(skocdop))
     end
 
     it 'updates only events from given semester' do
       event.update(semester: 'B132')
-      expect do
+      expect {
         described_class.perform(faculty_semester: faculty_semester)
         event.refresh
-      end.not_to change(event, :teacher_ids)
+      }.not_to change(event, :teacher_ids)
     end
 
     it 'updates only events from given faculty' do
       event.update(faculty: 13_000)
-      expect do
+      expect {
         described_class.perform(faculty_semester: faculty_semester)
         event.refresh
-      end.not_to change(event, :teacher_ids)
+      }.not_to change(event, :teacher_ids)
     end
 
     it 'changes updated_at column' do
-      expect do
+      expect {
         described_class.perform(faculty_semester: faculty_semester)
         event.refresh
-      end.to change(event, :updated_at)
+      }.to change(event, :updated_at)
     end
 
     context 'with not changed event' do
       let(:event) { Fabricate(:event, parallel: parallel, semester: faculty_semester.code, faculty: faculty_semester.faculty, teacher_ids: %w(skocdop), student_ids: %w(vomackar dude2)) }
 
       it 'does not update records with unchanged data' do
-        expect do
+        expect {
           described_class.perform(faculty_semester: faculty_semester)
           event.refresh
-        end.not_to change(event, :updated_at)
+        }.not_to change(event, :updated_at)
       end
     end
 
