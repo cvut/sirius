410b9eb2ddfb21279b650d8abb1cfc0ad8dda6a5
Tibor Szolár <tibor.szolar@seznam.cz>
2014-10-26 12:16:30 +0100

    Fix scheduling issue with seconds_since_midnight on DST transition day.

--- lib/sirius/semester_calendar.rb
+++ lib/sirius/semester_calendar.rb
@@ -11,8 +11,7 @@ module Sirius
 
     def plan(teaching_time)
       week_offset = teaching_time.week_offset(@first_week_parity)
-
-      scheduling_start = @teaching_period.starts_at + teaching_time.starts_at.seconds_since_midnight.seconds + week_offset
+      scheduling_start = combine_date_with_time(@teaching_period.starts_at, teaching_time.starts_at) + week_offset
       event_duration = (teaching_time.ends_at - teaching_time.starts_at)
 
       event_schedule = IceCube::Schedule.new(scheduling_start, duration: event_duration)
@@ -26,6 +25,10 @@ module Sirius
       week_frequency = 2 if teaching_time.parity != :both
 
       IceCube::Rule.weekly(week_frequency, :monday).day(teaching_time.day).until(@teaching_period.ends_at)
+    end
+
+    def combine_date_with_time(date, time)
+      Time.new(date.year, date.month, date.day, time.hour, time.min, time.sec)
     end
 
   end
