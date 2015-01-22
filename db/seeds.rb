cb192173a25c35e60ab7b69effc44811ce53568c
Tibor Szolár <tibor.szolar@seznam.cz>
2015-01-22 04:44:41 +0100

    Update semester data seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 require 'faculty_semester'
 
 FacultySemester.find_or_create(code: 'B141', faculty: 18000) do |s|
-  s.update_enabled = true
+  s.update_parallels = true
   s.first_week_parity = :odd
   s.starts_at = '2014-09-22'
   s.teaching_ends_at = '2014-12-20'
@@ -21,13 +21,26 @@ FacultySemester.find_or_create(code: 'B141', faculty: 18000) do |s|
 end
 
 FacultySemester.find_or_create(code: 'B141', faculty: 13000) do |s|
-  s.update_enabled = false
+  s.update_parallels = false
   s.first_week_parity = :odd
   s.starts_at = '2014-09-22'
   s.teaching_ends_at = '2014-12-20'
   s.exams_start_at = '2015-01-05'
   s.exams_end_at = '2015-02-14'
   s.ends_at = '2015-02-14'
+  s.hour_starts = %w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:30 14:30 15:15 16:15 17:00 18:00 18:45 19:45)
+  s.hour_duration = 45
+end
+
+FacultySemester.find_or_create(code: 'B142', faculty: 18000) do |s|
+  s.update_parallels = true
+  s.update_other = true
+  s.first_week_parity = :even
+  s.starts_at = '2015-02-16'
+  s.teaching_ends_at = '2015-05-16'
+  s.exams_start_at = '2015-05-18'
+  s.exams_end_at = '2015-06-27'
+  s.ends_at = '2015-09-21'
   s.hour_starts = %w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:30 14:30 15:15 16:15 17:00 18:00 18:45 19:45)
   s.hour_duration = 45
 end
