f9f7edb451b038c330a2b0c49cce31b86c111d32
Jakub Jirutka <jakub@jirutka.cz>
2015-02-10 13:53:49 +0100

    Fix Min validator to include the lower bound

--- lib/sirius_api/validators/min.rb
+++ lib/sirius_api/validators/min.rb
@@ -4,7 +4,7 @@ module SiriusApi
   module Validators
     class Min < ::Grape::Validations::SingleOptionValidator
       def validate_param!(attr_name, params)
-        unless params[attr_name].to_i > @option
+        unless params[attr_name].to_i >= @option
           raise Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "must be at least #{@option}"
         end
       end
