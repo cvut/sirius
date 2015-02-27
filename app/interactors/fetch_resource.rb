47662c60227d507ead10f437a8f1577569cc4d9d
Tibor Szolár <tibor.szolar@seznam.cz>
2015-02-27 13:27:26 +0100

    Customize FetchResource to allow interchangeable resource queries.

--- app/interactors/fetch_resource.rb
+++ app/interactors/fetch_resource.rb
@@ -1,30 +1,37 @@
 require 'interactors/kosapi_interactor'
+require 'sirius/faculty_semester_query'
 
 class FetchResource < KOSapiInteractor
 
   def perform(faculty_semester:, limit: 100, paginate: true)
     @faculty_semester = faculty_semester
     client = kosapi_client(faculty_semester)
-    @data = client.send(resource).where("course.faculty==#{faculty_semester.faculty};semester==#{faculty_semester.code}").limit(limit)
+    resource = client.send(resource_name)
+    @data = query_instance.apply(resource, faculty_semester: faculty_semester).limit(limit)
     @data.auto_paginate = paginate
   end
 
   def results
-    { resource => @data, faculty_semester: @faculty_semester }
+    { resource_name => @data, faculty_semester: @faculty_semester }
   end
 
   class << self
-    attr_accessor :resource
+    attr_accessor :resource, :query_instance
 
-    def [](resource)
+    def [](resource, query_instance: FacultySemesterQuery.new)
       Class.new(self).tap do |cls|
         cls.resource = resource
+        cls.query_instance = query_instance
       end
     end
   end
 
-  def resource
+  def resource_name
     self.class.resource
+  end
+
+  def query_instance
+    self.class.query_instance
   end
 
 end
---
+++ lib/sirius/faculty_semester_query.rb
@@ -0,0 +1,8 @@
+class FacultySemesterQuery
+
+  def apply(resource, faculty_semester: faculty_semester, **rest)
+    query = "course.faculty==#{faculty_semester.faculty};semester==#{faculty_semester.code}"
+    resource.where(query)
+  end
+
+end
