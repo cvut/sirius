88e162b87607a074277eb5f5129baf4bbdb7a26e
Jakub Jirutka <jakub@jirutka.cz>
2015-06-23 22:55:02 +0200

    Add URI parameter with_original_date to date filter

--- app/roles/date_filtered_dataset.rb
+++ app/roles/date_filtered_dataset.rb
@@ -4,12 +4,21 @@ require 'core_ext/then'
 ##
 # Filters dataset by starting date and end date.
 class DateFilteredDataset < RolePlaying::Role
-  # @param [Date,DateTime,String]
-  # @param [Date,DateTime,String]
-  # @return [Sequel::Dataset] filtered dataset
-  def filter_by_date(from: nil, to: nil)
+  ##
+  # @param from [Date, DateTime, String]
+  # @param to [Date, DateTime, String]
+  # @param with_original_date [Boolean] whether to include original +starts_at-
+  #        and +ends_at+ into the range.
+  # @return [Sequel::Dataset] a filtered dataset.
+  def filter_by_date(from: nil, to: nil, with_original_date: false)
 
-    self.then_if(from) { |d| d.where('starts_at >= ?', from) }
-        .then_if(to) { |d| d.where('ends_at <= ?', to) }
+    condition = ->(col, op) do
+      "#{col} #{op} :date".tap do |sql|
+        sql << " OR original_#{col} #{op} :date" if with_original_date
+      end
+    end
+
+    self.then_if(from) { |d| d.where(condition['starts_at', '>='], date: from) }
+        .then_if(to) { |d| d.where(condition['ends_at', '<='], date: to) }
   end
 end
