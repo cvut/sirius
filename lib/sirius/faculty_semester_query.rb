f2f931afc17433167caa5ded4349df2772e0281e
Jan Vlnas <git@jan.vlnas.cz>
2015-07-10 15:17:37 +0200

    Fix circular references in query classes
    Fixes #120

--- lib/sirius/faculty_semester_query.rb
+++ lib/sirius/faculty_semester_query.rb
@@ -1,6 +1,6 @@
 class FacultySemesterQuery
 
-  def apply(resource, faculty_semester: faculty_semester, **rest)
+  def apply(resource, faculty_semester:, **rest)
     query = "course.faculty==#{faculty_semester.faculty};semester==#{faculty_semester.code}"
     resource.where(query)
   end
