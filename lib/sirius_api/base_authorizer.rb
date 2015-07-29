c861c0f317843cf3b7fde88c646985cae3dfc7c2
Tibor Szolár <tibor.szolar@seznam.cz>
2015-07-29 21:00:37 +0200

    Document BaseAuthorizer public methods.

--- lib/sirius_api/base_authorizer.rb
+++ lib/sirius_api/base_authorizer.rb
@@ -14,32 +14,69 @@ module SiriusApi
     SCOPE_BASE = 'urn:ctu:oauth:sirius.'.freeze
 
     class << self
+      # Sets scope(s) for access rule definitions (defined by `.permit`).
+      #
+      # Calling `.permit` inside given block will define new access rule
+      # for all specified scopes.
+      #
+      # @param scopes [*String] One or more scopes for access rule definition.
+      #
       def scope(*scopes, &block)
         @current_scopes = scopes.map { |it| SCOPE_BASE + it }
         yield
       end
 
+      # Defines a new access rule.
+      #
+      # This should be only called inside the `.scope` block because a scope definition
+      # is required for every rule definition.
+      #
+      # @param http_method [Symbol] lowercase name of http method for which is the rule defined, e.g. :get, :post ...
+      # @param url [String] URL template for which is the rule defined, e.g. '/events/:id'
+      # @option options [Proc] :only Proc for optionally restricting the defined access rule.
+      #   If set, the access will be only allowed if the block return value is true.
+      #
       def permit(http_method, url, options = {})
         @current_scopes.each do |scope|
           (scope_registry[scope] ||= []) << Permission.new(http_method, url, options)
         end
       end
 
+      # Retrieves all rules defined by permission DSL.
       def scope_registry
         @scope_registry ||= {}
       end
     end
 
+    # Creates a new authorizer.
+    #
+    # @param current_user [String] Username of user currently accessing the application.
+    #
     def initialize(current_user)
       @current_user = current_user
     end
 
+    # Performs access check for a http request.
+    #
+    # Access check is done by searching for an access rule matching the current request.
+    # The set of searched rules is limited by specified scopes. If no matching rule is found,
+    # access is denied, otherwise matching rule is evaluated and access is granted / denied
+    # according to rule parameters.
+    #
+    # @param scopes [Array] Collection scopes strings for current user / application.
+    #   Scopes don't have to be prefixed by default URN prefix (will be added automatically if missing).
+    # @param http_method [Symbol] Lowercase name of currently performed http method, e.g. :get, :post ...
+    # @param url [String] URL template for current request, e.g. '/events/:id'
+    # @param route_params [Hash] Map of route parameters and their values.
+    #
     def authorize_request!(scopes, http_method, url, route_params = {})
       prefixed_scopes = scopes.map { |scope| prepend_prefix_if_missing(scope, SCOPE_BASE) }
       unless check_access(prefixed_scopes, http_method, url, route_params)
         raise SiriusApi::Errors::Authorization, "Access not permitted for #{http_method} #{url} with scopes=#{prefixed_scopes}"
       end
     end
+
+    private
 
     def check_access(scopes, http_method, url, route_params)
       checked_permission = Permission.new(http_method, url)
@@ -49,8 +86,6 @@ module SiriusApi
         permission_allowed?(scope_permissions, checked_permission, request_options)
       end
     end
-
-    private
 
     def build_options(scopes, http_method, url, route_params)
       {
