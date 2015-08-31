eac30d582b35b1360541a5e718f69e7837fbb256
Jan Vlnas <git@jan.vlnas.cz>
2015-08-31 18:54:21 +0200

    Add faculty/semesters B151 to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ std_hour_starts = Sequel.pg_array(%w(7:30 8:15 9:15 10:00 11:00 11:45 12:45 13:3
                   :time)
 
 FacultySemester.find_or_create(code: 'B141', faculty: 18000) do |s|
-  s.update_parallels = true
+  s.update_parallels = false
   s.first_week_parity = :odd
   s.starts_at = '2014-09-22'
   s.teaching_ends_at = '2014-12-20'
@@ -37,7 +37,7 @@ FacultySemester.find_or_create(code: 'B141', faculty: 13000) do |s|
 end
 
 fitb142 = FacultySemester.find_or_create(code: 'B142', faculty: 18000) do |s|
-  s.update_parallels = true
+  s.update_parallels = false
   s.update_other = true
   s.first_week_parity = :even
   s.starts_at = '2015-02-16'
@@ -45,6 +45,32 @@ fitb142 = FacultySemester.find_or_create(code: 'B142', faculty: 18000) do |s|
   s.exams_start_at = '2015-05-18'
   s.exams_end_at = '2015-06-27'
   s.ends_at = '2015-09-21'
+  s.hour_starts = std_hour_starts
+  s.hour_duration = 45
+end
+
+fitb151 = FacultySemester.find_or_create(code: 'B151', faculty: 18000) do |s|
+  s.update_parallels = true
+  s.update_other = true
+  s.first_week_parity = :odd
+  s.starts_at = '2015-10-05' # FIXME: actual start is on 01
+  s.teaching_ends_at = '2016-01-10'
+  s.exams_start_at = '2016-01-11'
+  s.exams_end_at = '2016-02-20'
+  s.ends_at = '2016-02-21'
+  s.hour_starts = std_hour_starts
+  s.hour_duration = 45
+end
+
+felb151 = FacultySemester.find_or_create(code: 'B151', faculty: 13000) do |s|
+  s.update_parallels = true
+  s.update_other = true
+  s.first_week_parity = :even
+  s.starts_at = '2015-10-01'
+  s.teaching_ends_at = '2016-01-17'
+  s.exams_start_at = '2016-01-18'
+  s.exams_end_at = '2016-02-21'
+  s.ends_at = '2016-02-21'
   s.hour_starts = std_hour_starts
   s.hour_duration = 45
 end
@@ -68,7 +94,62 @@ periods = [
     starts_at: '2015-06-29',
     ends_at: '2015-08-28',
     faculty_semester: fitb142,
-  }
+  },
+  ### B151
+  ## FIT
+  {
+    type: :teaching,
+    starts_at: '2015-10-05',
+    ends_at: '2015-12-22',
+    first_week_parity: :odd,
+    faculty_semester: fitb151,
+  },
+  {
+    type: :holiday,
+    starts_at: '2015-12-23',
+    ends_at: '2016-01-03',
+    faculty_semester: fitb151,
+  },
+  {
+    type: :teaching,
+    starts_at: '2016-01-04',
+    ends_at: '2016-01-10',
+    first_week_parity: :even,
+    faculty_semester: fitb151,
+  },
+  {
+    type: :exams,
+    starts_at: '2016-01-11',
+    ends_at: '2016-02-20',
+    faculty_semester: fitb151,
+  },
+  ## FEL
+  {
+    type: :teaching,
+    starts_at: '2015-10-01',
+    ends_at: '2015-12-22',
+    first_week_parity: :odd,
+    faculty_semester: felb151,
+  },
+  {
+    type: :holiday,
+    starts_at: '2015-12-23',
+    ends_at: '2016-01-03',
+    faculty_semester: felb151,
+  },
+  {
+    type: :teaching,
+    starts_at: '2016-01-04',
+    ends_at: '2016-01-17',
+    first_week_parity: :even,
+    faculty_semester: felb151,
+  },
+  {
+    type: :exams,
+    starts_at: '2016-01-18',
+    ends_at: '2016-02-21',
+    faculty_semester: felb151,
+  },
 ]
 
 periods.each do |period|
