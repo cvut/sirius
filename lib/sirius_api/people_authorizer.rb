0a0ee4d0702db71610b1e90bf55ef21cee663623
Jan Vlnas <git@jan.vlnas.cz>
2015-09-16 15:38:10 +0200

    Implement PeopleAuthorizer

---
+++ lib/sirius_api/people_authorizer.rb
@@ -0,0 +1,22 @@
+require 'sirius_api/base_authorizer'
+require 'sirius_api/scopes'
+require 'sirius_api/errors'
+
+module SiriusApi
+  class PeopleAuthorizer < BaseAuthorizer
+
+    scope Scopes::READ_PERSONAL do
+      permit :get, '/people/:username', only: ->(opts) { opts[:current_user] == opts[:target_user] }
+    end
+
+    scope Scopes::READ_ALL do
+      permit :get, '/people'
+      permit :get, '/people/:username'
+    end
+
+    def initialize(current_user)
+      super
+    end
+
+  end
+end
---
+++ spec/lib/sirius_api/people_authorizer_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'sirius_api/people_authorizer'
+require 'sirius_api/user'
+require 'sirius_api/scopes'
+
+describe SiriusApi::PeopleAuthorizer do
+
+  let(:scope) { SiriusApi::Scopes::READ_PERSONAL }
+  let(:user) { SiriusApi::User.new('ALICE', scope) }
+  subject(:authorizer) { described_class.new(user) }
+
+  describe '#authorize_request!' do
+
+    context 'for READ_PERSONAL scope' do
+      it 'allows request to personal endpoint' do
+        expect {
+          authorizer.authorize_request!(:get, '/people/:username', {username: 'ALICE'})
+        }.not_to raise_error
+      end
+
+      it 'denies request to endpoint for other user' do
+        expect {
+          authorizer.authorize_request!(:get, '/people/:username', {username: 'BOB'})
+        }.to raise_error(SiriusApi::Errors::Authorization)
+      end
+
+      it 'denies request to the index endpoint' do
+        expect {
+          authorizer.authorize_request!(:get, '/people')
+        }.to raise_error(SiriusApi::Errors::Authorization)
+      end
+    end
+
+
+    context 'for READ_ALL scope' do
+      let(:scope) { SiriusApi::Scopes::READ_ALL }
+
+      it 'allows request to individual endpoints' do
+        expect {
+          authorizer.authorize_request!(:get, '/people/:username', {username: 'BOB'})
+        }.not_to raise_error
+      end
+
+      it 'allows request to index endpoint' do
+        expect { authorizer.authorize_request!(:get, '/people') }.not_to raise_error
+      end
+    end
+  end
+end
