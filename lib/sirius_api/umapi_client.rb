e112ef25724b90e856fa0e73d836598cf8b7d642
Jakub Jirutka <jakub@jirutka.cz>
2015-10-05 22:00:42 +0200

    Refactor umapi_client and make it more robust

--- lib/sirius_api/umapi_client.rb
+++ lib/sirius_api/umapi_client.rb
@@ -6,44 +6,44 @@ require 'corefines'
 module SiriusApi
   class UmapiClient
 
-    using Corefines::Hash::rekey
-
-    UMAPI_PEOPLE_URI = Config.umapi_people_uri
+    using Corefines::Object::in?
 
     def initialize
-      client_id = Config.oauth_client_id
-      client_secret = Config.oauth_client_secret
-      auth_uri = Config.oauth_auth_uri
-      token_uri = Config.oauth_token_uri
       @client = OAuth2::Client.new(
-        client_id,
-        client_secret,
-        site: UMAPI_PEOPLE_URI,
-        authorize_url: auth_uri,
-        token_url: token_uri,
+        Config.oauth_client_id,
+        Config.oauth_client_secret,
+        site: Config.umapi_people_uri,
+        authorize_url: Config.oauth_auth_uri,
+        token_url: Config.oauth_token_uri,
         raise_errors: false
       )
     end
 
-    def user_has_roles?(username, roles, operator: 'all')
-      roles_param = roles.join(',')
-      user_uri = "#{UMAPI_PEOPLE_URI}/#{username}/roles?#{operator}=#{roles_param}"
+    def user_has_roles?(username, roles, operator: :all)
+      fail ArgumentError, "Parameter 'username' must not be blank." if username.blank?
+      fail ArgumentError, "Parameter 'roles' must not be empty." if roles.nil? || roles.empty?
+
+      unless operator.to_s.in? %(all any none)
+        fail ArgumentError, "Operator must be 'all', 'any', or 'none'."
+      end
+
+      user_uri = "#{@client.site}/#{username}/roles?#{operator}=#{roles.join(',')}"
       resp = token.request(:head, user_uri)
 
-      return true if resp.status == 200
-      return false if resp.status == 404
-      raise "Invalid response for #{user_uri} with status #{resp.status}."
+      case resp.status
+      when 200 then true
+      when 404 then false
+      else raise "Invalid response for #{@client.site}/#{user_uri} with status #{resp.status}."
+      end
     end
 
     private
 
     def token
-      authenticate if !@token || @token.expired?
+      if @token.nil? || @token.expired?
+        @token = @client.client_credentials.get_token
+      end
       @token
-    end
-
-    def authenticate
-      @token = @client.client_credentials.get_token
     end
   end
 end
--- spec/lib/sirius_api/umapi_client_spec.rb
+++ spec/lib/sirius_api/umapi_client_spec.rb
@@ -3,39 +3,69 @@ require 'sirius_api/umapi_client'
 
 describe SiriusApi::UmapiClient, :vcr do
 
+  subject(:client) { described_class.new }
   let(:username) { 'szolatib' }
 
   describe '#user_has_roles?' do
+
+    context 'with blank username' do
+      it 'raises ArgumentError' do
+        [nil, '', '  '].each do |username|
+          expect { client.user_has_roles? username, ['FOO'] }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context 'with empty roles' do
+      it 'raises ArgumentError' do
+        [nil, [], Set.new].each do |roles|
+          expect { client.user_has_roles? username, roles }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context 'with invalid operator' do
+      it 'raises ArgumentError' do
+        expect {
+          client.user_has_roles? username, ['FOO'], operator: 'xyz'
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+
+    # TODO: The following crap must be rewritten! It's extremly bad idea to test it against roles
+    #       of a real user in production Umapi!
+
     it 'checks whether user has a role assigned' do
-      expect(subject.user_has_roles?(username, ['B-00000-ZAMESTNANEC'])).to be_truthy
+      expect(client.user_has_roles?(username, ['B-00000-ZAMESTNANEC'])).to be_truthy
     end
 
     it 'checks whether user has a role not assigned' do
-      expect(subject.user_has_roles?(username, ['X-FOO-BAR'])).to be_falsey
+      expect(client.user_has_roles?(username, ['X-FOO-BAR'])).to be_falsey
     end
 
     it 'checks whether user has ALL of multiple roles assigned (positive case)' do
-      expect(subject.user_has_roles?(username, ['B-00000-ZAMESTNANEC', 'B-18000-ZAMESTNANEC'])).to be_truthy
+      expect(client.user_has_roles?(username, ['B-00000-ZAMESTNANEC', 'B-18000-ZAMESTNANEC'])).to be_truthy
     end
 
     it 'checks whether user has ALL of multiple roles (negative case)' do
-      expect(subject.user_has_roles?(username, ['NOT-A-ROLE', 'B-18000-ZAMESTNANEC'])).to be_falsey
+      expect(client.user_has_roles?(username, ['NOT-A-ROLE', 'B-18000-ZAMESTNANEC'])).to be_falsey
     end
 
     it 'checks whether user has ANY roles (positive case)' do
-      expect(subject.user_has_roles?(username, ['NOT-A-ROLE', 'B-18000-ZAMESTNANEC'], operator: 'any')).to be_truthy
+      expect(client.user_has_roles?(username, ['NOT-A-ROLE', 'B-18000-ZAMESTNANEC'], operator: 'any')).to be_truthy
     end
 
     it 'checks whether user has ANY roles (negative case)' do
-      expect(subject.user_has_roles?(username, ['NOT-A-ROLE', 'X-FOO-BAR'], operator: 'any')).to be_falsey
+      expect(client.user_has_roles?(username, ['NOT-A-ROLE', 'X-FOO-BAR'], operator: 'any')).to be_falsey
     end
 
     it 'checks whether user has NONE of multiple roles assigned (positive case)' do
-      expect(subject.user_has_roles?(username, ['NOT-A-ROLE', 'X-FOO-BAR'], operator: 'none')).to be_truthy
+      expect(client.user_has_roles?(username, ['NOT-A-ROLE', 'X-FOO-BAR'], operator: 'none')).to be_truthy
     end
 
     it 'checks whether user has NONE of multiple roles assigned (negative case)' do
-      expect(subject.user_has_roles?(username, ['B-00000-ZAMESTNANEC', 'X-FOO-BAR'], operator: 'none')).to be_falsey
+      expect(client.user_has_roles?(username, ['B-00000-ZAMESTNANEC', 'X-FOO-BAR'], operator: 'none')).to be_falsey
     end
   end
 end
