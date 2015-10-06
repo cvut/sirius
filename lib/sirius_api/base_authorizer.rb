f2cbe0cf3087a0572af89d7691e870d97b827ae0
Jakub Jirutka <jakub@jirutka.cz>
2015-10-06 21:00:30 +0200

    Fix code-style and docs in BaseAuthorizer

--- lib/sirius_api/base_authorizer.rb
+++ lib/sirius_api/base_authorizer.rb
@@ -2,6 +2,7 @@ require 'sirius_api/errors'
 require 'sirius_api/scopes'
 
 module SiriusApi
+
   Permission = Struct.new(:http_method, :url, :options) do
     def ==(other)
       http_method == other.http_method && url == other.url
@@ -13,10 +14,11 @@ module SiriusApi
     attr_reader :current_user
 
     class << self
+      ##
       # Sets scope(s) for access rule definitions (defined by `.permit`).
       #
-      # Calling `.permit` inside given block will define new access rule
-      # for all specified scopes.
+      # Calling `.permit` inside the given block will define a new access rule for all
+      # the specified scopes.
       #
       # @param scopes [*String] One or more scopes for access rule definition.
       #
@@ -25,15 +27,17 @@ module SiriusApi
         yield
       end
 
+      ##
       # Defines a new access rule.
       #
-      # This should be only called inside the `.scope` block because a scope definition
+      # This should be only called inside the `.scope` block, because a scope definition
       # is required for every rule definition.
       #
-      # @param http_method [Symbol] lowercase name of http method for which is the rule defined, e.g. :get, :post ...
-      # @param url [String] URL template for which is the rule defined, e.g. '/events/:id'
+      # @param http_method [Symbol] Lowercase name of HTTP method for which is the rule defined,
+      #   e.g. `:get`, `:post` etc.
+      # @param url [String] URL template for which is the rule defined, e.g. `/events/:id`.
       # @option options [Proc] :only Proc for optionally restricting the defined access rule.
-      #   If set, the access will be only allowed if the block return value is true.
+      #   If set, the access will be only allowed if the block returns true.
       #
       def permit(http_method, url, options = {})
         @current_scopes.each do |scope|
@@ -41,36 +45,41 @@ module SiriusApi
         end
       end
 
-      # Retrieves all rules defined by permission DSL.
+      ##
+      # @return [Hash] all rules defined by permission DSL.
       def scope_registry
         @scope_registry ||= {}
       end
     end
 
+    ##
     # Creates a new authorizer.
     #
-    # @param current_user [SiriusApi::User] User object representing user currently accessing the application.
+    # @param current_user [SiriusApi::User] User object representing the user currently accessing
+    #   the application.
     #
     def initialize(current_user)
       @current_user = current_user
     end
 
-    # Performs access check for a http request.
+    ##
+    # Performs access check for an HTTP request.
     #
     # Access check is done by searching for an access rule matching the current request.
     # The set of searched rules is limited by specified scopes. If no matching rule is found,
     # access is denied, otherwise matching rule is evaluated and access is granted / denied
     # according to rule parameters.
     #
-    # @param scopes [Array] Collection scopes strings for current user / application.
-    #   Scopes don't have to be prefixed by default URN prefix (will be added automatically if missing).
-    # @param http_method [Symbol] Lowercase name of currently performed http method, e.g. :get, :post ...
-    # @param url [String] URL template for current request, e.g. '/events/:id'
+    # @param http_method [Symbol] Lowercase name of currently performed HTTP method,
+    #   e.g. `:get`, `:post` etc.
+    # @param url [String] URL template for current request, e.g. `/events/:id`.
     # @param route_params [Hash] Map of route parameters and their values.
+    # @raise [SiriusApi::Errors::Authorization] if access is denied.
     #
     def authorize_request!(http_method, url, route_params = {})
       unless check_access(current_user.scopes, http_method, url, route_params)
-        raise SiriusApi::Errors::Authorization, "Access not permitted for #{current_user} on #{http_method} #{url}."
+        fail SiriusApi::Errors::Authorization,
+          "Access not permitted for #{current_user} on #{http_method} #{url}."
       end
     end
 
@@ -79,6 +88,7 @@ module SiriusApi
     def check_access(scopes, http_method, url, route_params)
       checked_permission = Permission.new(http_method, url)
       request_options = build_options(scopes, http_method, url, route_params)
+
       scopes.any? do |scope|
         scope_permissions = self.class.scope_registry[scope] || []
         permission_allowed?(scope_permissions, checked_permission, request_options)
@@ -98,7 +108,9 @@ module SiriusApi
 
     def permission_allowed?(scope_permissions, checked_permission, request_options)
       matching_permission = scope_permissions.find { |it| it == checked_permission }
+
       return false unless matching_permission
+
       only_block = matching_permission.options[:only]
       only_block.nil? || instance_exec(request_options, &only_block)
     end
