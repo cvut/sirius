b026ffbc7311aea1f2b66b77b4a639fc04277c35
Tibor Szolár <tibor.szolar@seznam.cz>
2015-10-08 16:39:12 +0200

    Add specs for PlannedSemesterPeriod first_day_override behaviour

--- spec/roles/planned_semester_period_spec.rb
+++ spec/roles/planned_semester_period_spec.rb
@@ -3,13 +3,10 @@ require 'period'
 require 'roles/planned_semester_period'
 
 describe PlannedSemesterPeriod do
-  subject(:period) { described_class.new(
-    Fabricate(:teaching_semester_period,
-                    starts_at: Time.parse('17.2.2014'),
-                    ends_at: Time.parse('16.5.2014'),
-                    first_week_parity: :even)
-  )}
-  let(:teaching_time) { Sirius::TeachingTime.new(teaching_period: Period.new(Time.parse('14:30'), Time.parse('16:00')), parity: :both, day: :tuesday) }
+
+  subject(:period) { create_period('17.2.2014', '16.5.2014') }
+  let(:teaching_day) { :tuesday }
+  let(:teaching_time) { Sirius::TeachingTime.new(teaching_period: Period.new(Time.parse('14:30'), Time.parse('16:00')), parity: :both, day: teaching_day) }
 
   it 'plans weekly event into semester calendar correctly' do
     events = period.plan(teaching_time)
@@ -34,9 +31,42 @@ describe PlannedSemesterPeriod do
     expect(events.length).to be 7
   end
 
+  context 'with first_day_override' do
+    subject(:period) { create_period('22.12.2015', '22.12.2015', first_day_override: 3) }
+
+    it 'does not plan events from other weekdays' do
+      events = period.plan(teaching_time)
+      expect(events).to be_empty
+    end
+
+    context 'for matching teaching time weekday' do
+      let(:teaching_day) { :wednesday }
+
+      it 'plans events from overriding day' do
+        events = period.plan(teaching_time)
+        expect(events).to contain_exactly(Period.parse('22.12.2015 14:30', '22.12.2015 16:00'))
+      end
+
+      it 'does not plan teaching times with opposite parity' do
+        teaching_time.parity = :odd
+        events = period.plan(teaching_time)
+        expect(events).to be_empty
+      end
+    end
+  end
+
   it 'returns two Time instances in Period' do
     event = period.plan(teaching_time).first
     expect(event.starts_at).to be_an_instance_of(Time)
     expect(event.ends_at).to be_an_instance_of(Time)
+  end
+
+  def create_period(starts_at, ends_at, first_day_override: nil)
+    period = Fabricate(:teaching_semester_period,
+                      starts_at: Time.parse(starts_at),
+                      ends_at: Time.parse(ends_at),
+                      first_week_parity: :even,
+                      first_day_override: first_day_override)
+    described_class.new(period)
   end
 end
