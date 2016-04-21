a73870265de20344b4c09f68fcba57015d3d8a35
Tibor Szolár <tibor.szolar@seznam.cz>
2016-04-21 17:14:10 +0200

    Add TeacherTimetableImport specs

---
+++ spec/actors/teacher_timetable_import_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'actors/teacher_timetable_import'
+
+describe TeacherTimetableImport do
+  include ActorHelper
+
+  let(:faculty_semester) { Fabricate(:faculty_semester) }
+  subject(:import) { described_class.new(faculty_semester) }
+
+  describe '#run!' do
+    it 'keeps the actor alive' do
+      import.run!
+      expect(import).to be_alive
+    end
+  end
+
+  describe 'shutdown!' do
+    it 'terminates the actor' do
+      import.shutdown!
+      expect(import).to be_dead
+    end
+  end
+end
