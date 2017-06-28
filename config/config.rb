34a230314147c28a0f9c40976cb9f87b13d82c12
Jakub Jirutka <jakub@jirutka.cz>
2017-06-28 17:06:07 +0200

    Rename config variable "db_pool" to "db_pool_max_size"

--- config/config.rb
+++ config/config.rb
@@ -26,7 +26,7 @@ module Config
 
   # Override -- value is returned or the set default.
   override :database_timeout,       10,               int
-  override :db_pool,                5,                int
+  override :db_pool_max_size,       5,                int
   override :deployment,             'production',     string
   override :elastic_prefix,         'sirius',         string
   override :force_ssl,              true,             bool
