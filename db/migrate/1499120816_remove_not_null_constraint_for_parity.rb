0390e49d1202bfc1c96910d315f8accccd1ac8cc
zidekja2 <zidekja2@fel.cvut.cz>
2020-09-12 14:24:54 +0200

    timetable_slot db table - removed not null constraints.

--- db/migrate/1499120816_remove_not_null_constraint_for_parity.rb
+++ db/migrate/1499120816_remove_not_null_constraint_for_parity.rb
@@ -2,6 +2,8 @@ Sequel.migration do
   change do
     alter_table(:timetable_slots) do
       set_column_allow_null :parity
+      set_column_allow_null :first_hour
+      set_column_allow_null :duration
     end
   end
 end
