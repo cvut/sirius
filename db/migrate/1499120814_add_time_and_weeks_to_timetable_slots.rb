61baa5421afc329f562f8f71b2a27ef441dcb154
zidekja2 <zidekja2@fel.cvut.cz>
2020-09-27 18:12:13 +0200

    Changing migration script of timetable_slot table to make columns of type "time without time zone" instead of datetime.

--- db/migrate/1499120814_add_time_and_weeks_to_timetable_slots.rb
+++ db/migrate/1499120814_add_time_and_weeks_to_timetable_slots.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
   change do
     alter_table(:timetable_slots) do
-      add_column :start_time, DateTime, default: nil, null: true
-      add_column :end_time, DateTime, default: nil, null: true
+      add_column :start_time, Time, only_time: true, default: nil, null: true
+      add_column :end_time, Time, only_time: true, default: nil, null: true
       add_column :weeks, String, default: nil, null: true
     end
   end
