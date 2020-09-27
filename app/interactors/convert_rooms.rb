f552d28baedead16df678f1963682020ce098e7f
zidekja2 <zidekja2@fel.cvut.cz>
2020-09-27 18:36:40 +0200

    convert_rooms optimized due to code review

--- app/interactors/convert_rooms.rb
+++ app/interactors/convert_rooms.rb
@@ -4,35 +4,15 @@ class ConvertRooms
   include Interpipe::Interactor
 
   def perform(kosapi_rooms:, **options)
-    @rooms = []
-    kosapi_rooms.each do |room|
-      converted_room = convert_room(room)
-
-      unless converted_room.nil?
-        @rooms << converted_room
-      end
+    @rooms = @rooms = kosapi_rooms.reduce([]) do |rooms, room|
+      next rooms if room.link_title == 'no-title'
+      rooms << Room.new(kos_code: room.link_id)
     end
-
     @options = options
   end
 
   def results
     { rooms: @rooms }.merge(@options)
-  end
-
-  private
-  def convert_room(room)
-    return if room.link_title == 'no-title'
-    room_code = room.link_id
-    Room.new(kos_code: room_code)
-  end
-
-  def extract_rooms(fetched_data)
-    {}.tap do |rooms|
-      fetched_data.lazy.map(&:room).reject(&:nil?).each do |room|
-        rooms[room.link_id] ||= room
-      end
-    end.values
   end
 
 end
